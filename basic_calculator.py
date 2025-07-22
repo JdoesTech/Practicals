a5c62b53219eb79c0060b11259066d1d033bb025
JdoesTech <onyangoocholla1139@gmail.com>
2025-07-22 18:10:49 +0300

    Create New Repo

---
+++ basic_calculator.py
@@ -0,0 +1,19 @@
+def switch_option():
+    option = int(input("Select your operation"))
+    match option:
+        case 1:
+            return (a+b)
+        case 2:
+            return (a-b)
+        case 1:
+            return (a*b)
+        case 1:
+            return (a/b)
+        
+def operation(a,b):
+    a = int(input("Add first number"))
+    b = int(input("Add second number"))
+    switch_option
+
+        
+        
