b18741d3cbf9b0a6571c4e0147f56cf576a82d8e
JdoesTech <onyangoocholla1139@gmail.com>
2025-08-19 03:17:56 +0300

    final word. Files have been restored, the repo is now public

--- pyWk3/discount_calc.py
+++ pyWk3/discount_calc.py
@@ -4,13 +4,16 @@ def calculate_discount(price, discount_percentage):
         return final_price
     else:
         return price
-    
+        
     
 def main():
     price=float(input("Enter the item price : "))
     discount_percentage=float(input("Enter the discount percentage : "))
     final_price =calculate_discount(price,discount_percentage)
-    print(f"The price is {final_price}")
+    if final_price<price:
+        print(f"The price after discount is {final_price}")
+    else: 
+        print(f"No discount was applied. The price is {final_price}")
 
 main()
 
