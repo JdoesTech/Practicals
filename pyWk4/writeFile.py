fbfd4aca6ff58bc03d35907076f5ffb8656720fd
JdoesTech <onyangoocholla1139@gmail.com>
2025-08-26 09:36:11 +0300

    pyWk4#2

--- pyWk4/writeFile.py
+++ pyWk4/writeFile.py
@@ -3,14 +3,16 @@ filename = input("Enter the filename: ")
 try:
     with open(filename, 'r') as f:
         content = f.read()
-        print(content)
+        print(content + "\n")
 except Exception as e:
     print(f"Error reading file: {e}")
 else:
     try:
-        with open("modified_" + filename, 'w') as f:
+        with open("modified_" + filename, 'r+') as f:
             f.write(content.upper()) 
-        print("Modified file saved as:", "modified_" + filename)
+            f.seek(0)
+            print(f.read())
+        print("\n Modified file saved as:", "modified_" + filename)
     except Exception as e:
         print(f"Error writing file: {e}")
 
