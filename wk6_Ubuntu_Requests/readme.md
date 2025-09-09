d606581dfe2c3c6be75ec799682311eafd509410
JdoesTech <onyangoocholla1139@gmail.com>
2025-09-09 11:42:57 +0300

    Additional Changes

---
+++ wk6_Ubuntu_Requests/readme.md
@@ -0,0 +1,4 @@
+To run this file, fork or colne the repository
+Kindly install venv (for your machine's sake). Be sure to activate it as well
+Run pip install -r requirements.txt to download dependencies
+Have fun with it
--- wk6_Ubuntu_Requests/requestty.py
+++ wk6_Ubuntu_Requests/requestty.py
@@ -2,25 +2,54 @@ import requests
 import os
 from PIL import Image
 
-if os.path.isdir("Fetched_Images"):
-    print("Exists")
-else: 
-    os.mkdir("Fetched_Images")
+#Preliminaries: are done on instant loading
+#Don't require user input
+def welcome():
+    print("Welcome")
 
-
-print("Welcome")
-url = input("Paste picture link here")
-headers = {
+# Scrapper
+def scrape():
+    #check Directory
+    directory= "Fetched_Images"
+    if  not os.path.isdir(directory):
+        os.mkdir(directory)
+    #paste url link
+    url = input("Paste picture link here : ")
+    #Define Header parameters
+    headers = {
     "User-Agent": "Mozilla/5.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+    "Accept": "image/*,image/webp"
 }
-response = requests.get(url, headers=headers)
-if response.status_code == 200:
-    print("Image accessed successfully")
-else: 
-    print("Error code: ", response.status_code)
-if response.headers["content-type"] != "image/webp":
-    print("Please insert image link")
-print(response.headers.get("content-disposition", "No content-disposition header"))
+    #get response
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        print("Image accessed successfully")
+    else: 
+        print("Error code: ", response.status_code)
+    if response.headers["content-type"] != "image/webp":
+        print("Please insert image link")
+    #Obtain file name
+    cont_disposition =response.headers.get("content-disposition", "")
+    #("No content-disposition header").split("filename")[1])
+    #Assign file name
+    if "filename" in cont_disposition:
+        fname_raw= cont_disposition.split("filename")[1].strip('=')
+        Filename= fname_raw
+    else: 
+        Filename=url.split("/")[-1]
+    #print("From Here")
+    print(f"Successfully fetched : {Filename} ")
+    #save pic
+    filepath=os.path.join(directory, Filename)
+    with open(filepath, "wb") as f:
+        f.write(response.content)
+        print(f"image saved to : {filepath}")
+
+def main():
+    welcome()
+    scrape()
+
+main()
+
 
 
