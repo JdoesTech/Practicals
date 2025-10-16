a647fc9ea714a6dcde6ad5a0506fa87f5a675f5f
JdoesTech <onyangoocholla1139@gmail.com>
2025-10-16 19:09:01 +0300

    Rectifications on wk3's discount calculator

--- pyWk3/discount_calc.py
+++ pyWk3/discount_calc.py
@@ -7,13 +7,29 @@ def calculate_discount(price, discount_percentage):
         
     
 def main():
-    price=float(input("Enter the item price : "))
-    discount_percentage=float(input("Enter the discount percentage : "))
+    try:
+        price = float(input("Enter the item price: "))
+        discount_percentage = float(input("Enter the discount percentage: "))
+    except ValueError:
+        print("Invalid input. Please enter numeric values for price and discount.")
+        exit()
+        
+    try:
+        if price <0 or discount_percentage <0:
+            raise ValueError("Entered values must be non-negative.")
+        if discount_percentage >100:
+            raise ValueError("Discount percentage cannot exceed 100.")
+    except ValueError as ve:
+        print(ve)
+        exit()
+    
+        
     final_price =calculate_discount(price,discount_percentage)
     if final_price<price:
         print(f"The price after discount is {final_price}")
     else: 
-        print(f"No discount was applied. The price is {final_price}")
+        print(f"No discount was applied. The price is {final_price:.2f}")
+    
 
 main()
 
